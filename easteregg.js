511e6845fb854603f58262932babfdafd6f892ca
Aoook <61693887+Aoook@users.noreply.github.com>
2022-12-01 19:48:50 +0100

    Add files via upload

---
+++ easteregg.js
@@ -0,0 +1,9 @@
+var count = 0;
+  function buttonClick(){
+        count++;
+        if(count == 10){
+            window.open("https://www.youtube.com/watch?v=t99KH0TR-J4");
+            count=0;
+        }
+      }
+
